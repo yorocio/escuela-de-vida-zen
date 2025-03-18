c2a929964825c15673d3b516382bc813fa999477
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-03-18 12:02:52 -0300

    barra de navegacion mejorada

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -5,15 +5,25 @@ import logo from "../public/logo.png"
 
 const Navbar = () => {
   return (
-    <header className='flex justify-center items-center py-4 px-4 sm:px-10 font-[sans-serif] min-h-[70px] tracking-wide fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-sm bg-opacity-60 text-gray-500 shadow-sm'>
+    <header className='flex justify-between items-center py-4 px-4 sm:px-10 font-[sans-serif] min-h-[70px] tracking-wide fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur-sm bg-opacity-60 text-gray-500 shadow-sm'>
 
-      {/* Logo alineado a la izquierda */}
-      <Link href="/" className="absolute left-4">
+      {/* Logo alineado a la izquierda, ocultar en pantallas pequeñas y medianas */}
+      <Link href="/" className="hidden md:block absolute left-4">
         <Image src={logo} alt="logo" width={50} height={50} />
       </Link>
 
-      {/* Menú centrado */}
-      <nav className='flex flex-wrap justify-center gap-14'>
+      {/* Menú centrado, en pantallas grandes */}
+      <nav className='hidden md:flex justify-center gap-14 w-full'>
+        <Link href="/services" className='hover:text-gray-400 text-sm sm:text-base'>Servicios</Link>
+        <Link href="/courses" className='hover:text-gray-400 text-sm sm:text-base'>Cursos</Link>
+        <Link href="/about" className='hover:text-gray-400 text-sm sm:text-base'>Sobre Zen Shiatsu</Link>
+        <Link href="/contact" className='hover:text-gray-400 text-sm sm:text-base'>Contacto</Link>
+        <Link href="/blog" className='hover:text-gray-400 text-sm sm:text-base'>Blog</Link>
+        <Link href="/calendly" className='hover:text-gray-400 text-sm sm:text-base'>Turnos</Link>
+      </nav>
+
+      {/* Menú móvil, visible en pantallas pequeñas */}
+      <nav className='sm:hidden flex justify-center gap-4 w-full'>
         <Link href="/services" className='hover:text-gray-400 text-sm sm:text-base'>Servicios</Link>
         <Link href="/courses" className='hover:text-gray-400 text-sm sm:text-base'>Cursos</Link>
         <Link href="/about" className='hover:text-gray-400 text-sm sm:text-base'>Sobre Zen Shiatsu</Link>
