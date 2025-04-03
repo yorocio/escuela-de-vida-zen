bdbaee37e05f230caef1293889b526c9ee34c001
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-04-03 09:56:12 -0300

    installo AuthJS

--- front/components/Buttons/ButtonLogin.js
+++ front/components/Buttons/ButtonLogin.js
@@ -4,9 +4,16 @@ import React from 'react'
 const ButtonLogin = () => {
   return (
     <div>
-        <Link className='border border-rose-700' href='/'>Login/Iniciar Sesión</Link>
+        <Link className='border border-rose-700' href='/'>Iniciar Sesión</Link>
     </div>
   )
 }
 
 export default ButtonLogin
+
+
+//PARA INICIAR SESIÓN:
+//1. CREO PAGINA DE LOGIN (LISTO)
+//2. CREO EL FORMULARION CON EMAIL Y CONTRASEÑA (LISTO)
+//2.A) O INICIAR SESIÓN CON GOOGLE (FALTA)
+//3. HAGO UNA SOLICITUD POST A /api/auth (FALTA)
