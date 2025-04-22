540cdcdb71450ebbfc79785451b8a1185c0e217b
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-04-22 10:17:40 -0300

    uso resend de AuthJS para inicio de sesion

--- front/auth.js
+++ front/auth.js
@@ -1,9 +1,16 @@
 import NextAuth from "next-auth";
+import Resend from "next-auth/providers/resend";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./libs/mongo";
 
 const config = {
-    providers: [],
+    providers: [
+        Resend({
+            apiKey: process.env.RESEND_KEY,
+            from: "noreply@achalay.xyz",
+            name: "Email",
+        })
+    ],
     //MongoDBAdapter(clientPromise) conecta AuthJS con mi base de datos de MongoDB
     adapter: MongoDBAdapter(clientPromise)
 }
