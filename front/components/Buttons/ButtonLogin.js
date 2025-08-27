a4df78d1f7e13517dbe73ee903d6d9ff864fabb5
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-08-27 12:48:41 -0300

    personalizo el login para iniciar sesion con google o con email

--- front/components/Buttons/ButtonLogin.js
+++ front/components/Buttons/ButtonLogin.js
@@ -1,18 +1,33 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
+import { signIn } from 'next-auth/react'
 
 const ButtonLogin = ({ session, name }) => {
+  //para no repetir la ruta en varias partes del codigo creo una constante con la ruta
+  const dashboardUrl = '/dashboard';
+
   if (session) {
     return (
       //SI ESTA LOGUEADO MUESTRA EL BOTON EN EL DASHBOARD (si inicia sesion con google, aparecera el nombre sino solo !)
-      <div>
-        <Link className='border border-rose-700' href='/dashboard'>Bienvenido/a {session.user.name || "!"}</Link>
+      <div className='m-6'>
+        <Link className='border-2 border-rose-700 border-solid p-2' href={dashboardUrl}>Bienvenido/a {session.user.name || "!"}
+        </Link>
       </div>
     );
   }
 
   //SI NO ESTA LOGUEADO MUESTRA EL BOTON PARA INICIAR SESION 
-  return <button className='border border-green-800'>Iniciar Sesión</button>
+  return (
+    <button className=' m-6 border-2 border-green-800'
+      onClick={() => {
+        //primer parametro es el provider con el que vamos a iniciar sesion. Puede ser 'google' para iniciar sesion especificamente con google o 'undefined' para que pueda iniciar sesion con un correo electronico o google. 
+        //El segundo parametro es un obj que usaremos para personalizar lo que sucede durante el procesio de inicio de sesion. Hay muchas opciones que podemos personalizar, una es la callbackUrl que es la URL a la que queremos volver o dirigirnos despues de iniciar sesion. 
+        signIn('undefined', { callbackUrl: dashboardUrl })
+      }}>
+      Iniciar Sesión
+    </button>
+  );
 };
 
 export default ButtonLogin
