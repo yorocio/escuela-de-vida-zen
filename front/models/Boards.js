c472bbbcf6319ef50bd59eb1ec1e68f8cb704731
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-09-08 08:36:17 -0300

    nueva funcionalidad: schema y modelo a board

---
+++ front/models/Boards.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+//SCHEMA PARA BOARD
+const boardSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    }
+});
+
+//MODEL DE BOARD (si el modelo existe o crea uno nuevo)
+const Board = mongoose.models.Board || mongoose.model("Board", boardSchema);
+
+export default Board;
+
+//se puede reemplazar la linea 18 y 20 por: export default mongoose.model("Board", boardSchema)
