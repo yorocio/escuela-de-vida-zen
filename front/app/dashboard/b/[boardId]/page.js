7db3059c75f971d8ac82224836bc04cdefdf17ba
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2025-09-18 19:03:10 -0300

    prueba ruta dinamica para tablero

---
+++ front/app/dashboard/b/[boardId]/page.js
@@ -0,0 +1,8 @@
+export default function FeedbackBoard({ params }) {
+    const { boardId } = params;
+
+    //Obtener tablero con objetId 'boardId'
+
+
+    return <main className="my-28 p-2">{boardId}</main>
+}
